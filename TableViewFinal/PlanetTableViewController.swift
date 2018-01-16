619adc970cb4b39d1e2b9557fd71e815c87c15c3
MelissaGarrett <melissa_garrett@hotmail.com>
2018-01-15 21:41:07 -0600

    A lab to practice table views.

--- TableViewFinal/PlanetTableViewController.swift
+++ TableViewFinal/PlanetTableViewController.swift
@@ -9,4 +9,48 @@
 import UIKit
 
 class PlanetTableViewController: UITableViewController {
+    let planetCellIdentifier: String = "PlanetCell"
+    
+    var planets: [Planet] = []
+    
+    func generatePlanetInfo() {
+        let earth = Planet(name: "Earth", numberOfMoons: 1, fullOrbit: 365.26, facts: ["Earth is the only planet not named after a god.", "Earth has a powerful magnetic field.", "Earth was once believed to be the center of the universe."])
+        
+        let mars = Planet(name: "Mars", numberOfMoons: 2, fullOrbit: 687.0, facts: ["Mars and Earth have approximately the same landmass.", "Mars is home to the tallest mountain in the solar system.", "Pieces of Mars have fallen to Earth."])
+        
+        self.planets = [earth, mars]
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        generatePlanetInfo()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return planets.count
+    }
+    
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: planetCellIdentifier, for: indexPath) as! PlanetTableViewCell
+
+        cell.planetNameLabel.text = planets[indexPath.row].name
+        cell.numberOfMoonsLabel.text = String(planets[indexPath.row].numberOfMoons) + " Moon"
+        cell.numberOfDaysFullOrbitLabel.text = String(planets[indexPath.row].fullOrbit) + " days (full orbit)"
+        cell.numberOfFacts.text = String(planets[indexPath.row].facts.count) + " facts"
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destVC = segue.destination as? FactTableViewController, let indexPath = tableView.indexPathForSelectedRow {
+            destVC.planet = planets[indexPath.row]
+        }
+    }
+    
 }
